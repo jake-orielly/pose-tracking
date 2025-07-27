aa439283ed99e2466c50081fb23d6dab680a2bf6
Jake O'Rielly <jake.orielly@gmail.com>
2025-07-27 11:05:06 +0200

    Draw joints

--- main.py
+++ main.py
@@ -8,9 +8,30 @@ mp_pose = mp.solutions.pose
 webcamNumber = 1
 
 cap = cv2.VideoCapture(webcamNumber)
-while cap.isOpened():
-  ret, frame = cap.read()
-  cv2.imshow('Mediapipe Feed', frame)
+
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+  while cap.isOpened():
+    ret, frame = cap.read()
+
+    # Recolor image to RGB
+    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    image.flags.writeable = False
+
+    # Make detection
+    results = pose.process(image)
+
+    # Recolor back to BGR
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    # Render detections
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
+    cv2.imshow('Mediapipe Feed', image)
+
+    # Exit on q
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+      break
 
 cap.release()
 cv2.destroyAllWindows()
